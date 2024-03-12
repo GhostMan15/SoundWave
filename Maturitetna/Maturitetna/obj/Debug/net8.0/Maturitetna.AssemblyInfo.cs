acbdc5ccd11a3832616fd5d87411fae65330a570
GhostMan15 <farukuzicanin1@gmail.com>
2024-03-12 12:55:40 -0700

    vm

--- Maturitetna/Maturitetna/obj/Debug/net8.0/Maturitetna.AssemblyInfo.cs
+++ Maturitetna/Maturitetna/obj/Debug/net8.0/Maturitetna.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maturitetna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bf9a145c79122640838df1c9580b5ee70fb928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8f4fa00725bf65ce77681af6eeca8945c3ed03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maturitetna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maturitetna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
